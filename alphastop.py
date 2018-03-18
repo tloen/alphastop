6b178a94e09f327bb73d42bbb5643114d5b47c0c
Eric Wang <ejwang@stanford.edu>
2018-03-18 16:47:20 -0700

    streamlined sentence sorting interface

--- alphastop.py
+++ alphastop.py
@@ -65,6 +65,9 @@ def generate_two_words():
       break
   return np.stack((encode_word(s1), encode_word(s2))), [s1, s2]
 
+def sentence_to_words(sentence):
+  return sentence.split()
+
 x_i = tf.placeholder(
   dtype=tf.float32, 
   shape=(max_length, 27)
@@ -100,8 +103,6 @@ outputs, state = tf.nn.dynamic_rnn(
   )
 )
 
-init = tf.global_variables_initializer()
-
 sigma = 10.0
 
 score_i = outputs[0][-1][0]
@@ -109,13 +110,14 @@ score_j = outputs[1][-1][0]
 z = tf.multiply(-sigma, tf.subtract(score_i, score_j))
 p_ij = tf.divide(1.0, tf.add(1.0, tf.exp(z)))
 cost = tf.negative(tf.log(p_ij))
-opt = tf.train.GradientDescentOptimizer(0.001)
+opt = tf.train.AdamOptimizer(0.0001)
 train = opt.minimize(cost)
 
+init = tf.global_variables_initializer()
 with tf.Session() as sess:
   sess.run(init)
   moving_average_accuracy = 0.5
-  for t in range(100000):
+  for t in range(1000000):
     words, raws = generate_two_words()
     if raws[0] < raws[1]:
       i, j = 1, 0
@@ -124,8 +126,8 @@ with tf.Session() as sess:
     c, _ = sess.run([cost, train], feed_dict={x_i: words[i], x_j: words[j]})
 
     moving_average_accuracy = 0.999 * moving_average_accuracy + 0.001 * (c < 0.69)
-    if t % 100 == 0:
-      print(t, moving_average_accuracy)
+    if (t + 1) % 1000 == 0:
+      print(t + 1, moving_average_accuracy)
   def word_score(word):
     return sess.run(outputs, 
     feed_dict={
@@ -134,9 +136,17 @@ with tf.Session() as sess:
       x_i: encode_word(word),
       x_j: encode_word(word)
       })[0, -1, 0]
-  words = ["oh", "say", "can", "you", "see"]
-  pairs = [(-word_score(w), w) for w in words]
-  print(sorted(pairs))
+  def sort_sentence(sentence):
+    words = sentence_to_words(sentence)
+    pairs = [(word_score(w), w) for w in words]
+    p2 = sorted(pairs)
+    l = [w for s, w in p2]
+    return l
+  print(sort_sentence("well seymour i made it despite your directions"))
+  print(sort_sentence("ah superintendent chalmers welcome i hope youre ready for an unforgettable luncheon"))
+  saver = tf.train.Saver()
+  save_path = saver.save(sess, "./alphastop_params.ckpt")
+  print("Model saved in %s" % save_path)
 
 
 
